ee28f803322894052ce76b7789b5d728a09b1a3d
abhi <awadhawan18@gmail.com>
2019-03-18 02:47:43 +0530

    Custom search changes

--- app/src/main/java/com/example/alumnicellsystem/EnterPassword.java
+++ app/src/main/java/com/example/alumnicellsystem/EnterPassword.java
@@ -19,7 +19,7 @@ public class EnterPassword extends AppCompatActivity {
         setPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                startActivity(new Intent(getApplicationContext(), Dashboard.class));
             }
         });
     }
--- app/src/main/java/com/example/alumnicellsystem/SplashScreenActivity.java
+++ app/src/main/java/com/example/alumnicellsystem/SplashScreenActivity.java
@@ -23,7 +23,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                Intent homeIntent = new Intent(SplashScreenActivity.this, SignIn.class);
                 startActivity(homeIntent);
                 finish();
             }
