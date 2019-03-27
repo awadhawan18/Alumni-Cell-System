a6146617bc1276787275904fc37da8e5483298f4
abhi <awadhawan18@gmail.com>
2019-03-28 02:56:41 +0530

    Custom Search working

--- app/src/main/java/com/example/alumnicellsystem/YearwiseSearch.java
+++ app/src/main/java/com/example/alumnicellsystem/YearwiseSearch.java
@@ -1,8 +1,7 @@
 package com.example.alumnicellsystem;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -28,7 +27,7 @@ public class YearwiseSearch extends AppCompatActivity {
                 year = yearSP.getSelectedItem().toString();
                 //TODO: The Api call comes here
 
-                startActivity(new Intent(getApplicationContext(), AlumniList.class));
+                //startActivity(new Intent(getApplicationContext(), AlumniList.class));
             }
         });
     }
